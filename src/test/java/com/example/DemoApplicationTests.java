7383dda39789cde1330947371b172cc0bb9b0007
MounirLig <mounir.lig@gmail.com>
2020-01-16 11:48:32 +0100

    Nouveau Tests

--- src/test/java/com/example/DemoApplicationTests.java
+++ src/test/java/com/example/DemoApplicationTests.java
@@ -13,4 +13,47 @@ public class DemoApplicationTests {
 	public void contextLoads() {
 	}
 
+	@Test
+        public void shouldAnswerWithTrue()
+        {
+	logger.error("Starting App");
+        assertTrue( true );
+	logger.error("Closing App");
+        }
+
+	@Test
+        public void shouldAnswerWithTrue2()
+        {
+        assertTrue( true );
+        }
+
+	@Test
+        public void shouldAnswerWithTrue3()
+        {
+        assertTrue( true );
+        }
+
+	@Test
+        public void shouldAnswerWithTrue4()
+        {
+        assertTrue( false );
+        }
+
+	@Test
+        public void shouldAnswerWithTrue5()
+        {
+        assertTrue( false );
+        }
+
+
+        @BeforeClass public static void initialize() {
+        logger.error("Starting Tests");
+        }
+
+        @After public void ending() {
+        logger.error("ending tests");
+        }
+
+
+
 }
